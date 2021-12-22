63fb1c6c1d27f0dcf9bdd18ffba84aaf6b41adca
Jueri <jueri.keller@smail.th-koeln.de>
2021-12-22 13:32:05 +0000

    fix dataset processing bug

--- src/dataset.py
+++ src/dataset.py
@@ -23,7 +23,9 @@ from sklearn.model_selection import train_test_split  # type: ignore
 nltk.data.path.append(NLTK_DATA_PATH)
 
 
-def load_dataset(dataset_path: str, split_size: float=0.2, false_class_balance: float=1.0) -> tuple[pd.DataFrame, pd.DataFrame, pd.Array, pd.Array]:
+def load_dataset(
+    dataset_path: str, split_size: float = 0.2, false_class_balance: float = 1.0
+) -> tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
     """Import a prpared dataset, downsample the non claim class and return train and test splits.
 
     Args:
@@ -49,8 +51,6 @@ def load_dataset(dataset_path: str, split_size: float=0.2, false_class_balance: 
     return X_train, X_test, y_train, y_test
 
 
-
-
 def preprocess_dataset_2014():
     """Prepare the dataset IBM_Debater_(R)_CE-ACL-2014.v0 for model training."""
 
@@ -66,16 +66,18 @@ def preprocess_dataset_2014():
         claim_sentences = claim_sentences.apply(lambda x: re.escape(x)).to_list()
 
         # Prepare table
-        df = pd.read_csv(os.path.join(ARTICLE_PATH, article), delimiter="\t", names=["Text"], quoting=3)  # load article to table
+        df = pd.read_csv(
+            os.path.join(ARTICLE_PATH, article), delimiter="\t", names=["Text"], quoting=3
+        )  # load article to table
         df["Article"] = article_name  # add article name
         df["Sentence"] = df.apply(lambda x: sent_tokenize(x["Text"]), axis=1)  # split text to sentences
         df = df.explode("Sentence")  # each sentence one row
 
         # Clean text
-        df["Sentence"] = df["Sentence"].str.replace("[REF]", "", regex=False) 
+        df["Sentence"] = df["Sentence"].str.replace("[REF]", "", regex=False)
         df["Sentence"] = df["Sentence"].str.replace("[REF", "", regex=False)
         df["Sentence"] = df["Sentence"].str.replace(" .", ".", regex=False)
-        df = df[df["Sentence"].str.len()>5]  # delete "sentences" with only 5 chars
+        df = df[df["Sentence"].str.len() > 5]  # delete "sentences" with only 5 chars
 
         df = df.drop(["Text"], axis=1)  # drop original text column
 
@@ -94,14 +96,14 @@ def preprocess_dataset_2014():
 
 def preprocess_dataset_2018():
     """Prepare the dataset IBM_Debater_(R)_claim_sentences_search for model training."""
-    
-    names = ["id", "Article", "mc", "Sentence", "query_pattern", "score", "Claim", "url"]
-    data = pd.read_csv(os.path.join(DATASET_2018_DIR, "test_set.csv"), names=names)
-    data["Claim"] = data["Claim"].apply(lambda x: True if x==1 else False)
 
-        # Clean text
-    data["Sentence"] = data["Sentence"].str.replace("[REF]", "", regex=False) 
+    names = ["id", "topic", "Article", "Sentence", "query_pattern", "score", "Claim", "url"]
+    data = pd.read_csv(os.path.join(DATASET_2018_DIR, "test_set.csv"), names=names)
+    data["Claim"] = data["Claim"].apply(lambda x: True if x == 1 else False)
+
+    # Clean text
+    data["Sentence"] = data["Sentence"].str.replace("[REF]", "", regex=False)
     data["Sentence"] = data["Sentence"].str.replace("[REF", "", regex=False)
     data["Sentence"] = data["Sentence"].str.replace(" .", ".", regex=False)
     sentences = data[["Article", "Sentence", "Claim"]]
-    sentences.to_csv(DATASET_2018_PATH, index=False)    
+    sentences.to_csv(DATASET_2018_PATH, index=False)
